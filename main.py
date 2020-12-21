6fbdd7c8e495a56dc219fb5a39dc1418e63ddb34
AdiShirsath <adityashirsath4@gmail.com>
2020-12-21 16:31:23 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pyttsx3
+import PyPDF2
+
+# for opening pdf
+book = open('Class 11 - India Physical Environment.pdf', 'rb')
+pdf_reader = PyPDF2.PdfFileReader(book)
+pages = pdf_reader.numPages
+
+# setting up engine to speak
+speaker = pyttsx3.init()
+voices = speaker.getProperty('voices')
+speaker.setProperty('voice', voices[1].id)
+
+# engine reading pdf
+for num in range(9, pages):
+ page = pdf_reader.getPage(num)
+ text = page.extractText()
+ speaker.say(text)
+ speaker.runAndWait()
